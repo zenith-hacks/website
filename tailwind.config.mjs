3fda86af9cfb216a298535942f8cdebec33049fb
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-30 02:26:14 +0100

    Make CoC prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'selector',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
     container: {
-			center: true,
+      center: true,
       padding: '1rem',
-			screens: {
-				xl: '1024px'
-			}
-		},
-		extend: {
+      screens: {
+        xl: '1024px'
+      }
+    },
+    extend: {
       typography: {
         DEFAULT: {
           css: {
@@ -19,6 +19,6 @@ export default {
         }
       }
     },
-	},
-	plugins: [require('@tailwindcss/typography')],
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
